9cfa76b5a04fce8388093710668fb3fd2fc15c44
gjergjk71 <gjergjk71@gmail.com>
2018-08-05 14:18:25 +0200

    Created loginUser function

--- database.py
+++ database.py
@@ -3,6 +3,8 @@ import sqlite3
 def createDatabase(path):
 	conn = sqlite3.connect(path)
 	conn.close()
+	print("Database created or already existed")
+
 
 def createUserModel(path):
 	table = "User"
@@ -16,3 +18,20 @@ def createUserModel(path):
 	c.execute("CREATE TABLE IF NOT EXISTS {}({});".format(table,columns))
 	conn.commit()
 	conn.close()
+	print("Created User table")
+
+def loginUser(username,password,path):
+	conn = sqlite3.connect(path)
+	c = conn.cursor()
+	users = c.execute("SELECT * FROM User")
+	users = c.fetchall()
+	print(users)
+	conn.close()
+	for user in users:
+		print(user)
+		if user[1] == username:
+			if user[2] == password:
+				return True
+	return False
+
+#print(loginUser("gjergjk71","gjergji.123","db.sqlite3"))
