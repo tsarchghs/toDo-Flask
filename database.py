21fb837890bf189609ad4bbe175197d79d664789
gjergjk71 <gjergjk71@gmail.com>
2018-08-05 14:02:22 +0200

    Created createDatabase function

--- database.py
+++ database.py
@@ -1,15 +1,18 @@
 import sqlite3
 
+def createDatabase(path):
+	conn = sqlite3.connect(path)
+	conn.close()
 
 def createUserModel(path):
 	table = "User"
 	columns = """
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		username TEXT UNIQUE,
-		password TEXT,
+		password TEXT
 			 """
-	conn = db.connect(path)
+	conn = sqlite3.connect(path)
 	c = conn.cursor()
-	c.execute("CREATE TABLE IF NOT EXISTS {}({})".format(table,columns))
+	c.execute("CREATE TABLE IF NOT EXISTS {}({});".format(table,columns))
 	conn.commit()
 	conn.close()
